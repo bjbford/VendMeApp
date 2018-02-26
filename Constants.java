49bac04524febfaf1f0c555d02e834bd8a7d69f4
jtbartz <jtbartz@iastate.edu>
2018-02-26 16:26:21 +0000

    Upload New File

---
+++ Constants.java
@@ -0,0 +1,17 @@
+package ss_3.iastate.edu.vendme;
+
+/**
+ * Created by JB on 2/20/18.
+ */
+
+
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://proj-309-ss-3.cs.iastate.edu/android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"createMachine.php";
+
+
+
+}
